51d3470793c1e2761b0daf2131e752a03d6fc864
Muhammad jumshaid <jumshaid.khan.1222@gmail.com>
2024-12-09 06:42:14 -0800

    Second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,13 @@ const Navbar = () => {
   return (
     <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-34'>
      {/* For mobile screen */}
+     <div className='h-full  flex items-center justify-between'>
      <Link href="/">
-     Haseeb
+     <div className='text-2xl tracking-wide'>Haseeb</div>
+     
      </Link>
      <Menue/>
+     </div>
     </div>
   )
 }
