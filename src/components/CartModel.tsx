345e682a8ed8a8d5ec35db0f2aae90982da103e5
Muhammad jumshaid <jumshaid.khan.1222@gmail.com>
2024-12-18 07:15:24 -0800

    till 1.3 min

--- src/components/CartModel.tsx
+++ src/components/CartModel.tsx
@@ -1,10 +1,98 @@
 "use client"
 
 const CartModel = () => {
+
+  // const cartItems = false;
+  const cartItems = true;
+
   return (
-    <div className=''>
-      CartModel
+    <>   
+    
+     <div className='w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20'>
+    
+         {
+          !cartItems ?  <div className="">
+        Cart is Empty
+      </div>   
+       : <>
+       <h2 className="text-xl">Shopping Cart</h2>
+       <div className="flex flex-col gap-8">    
+
+       {/* ITEMS     */}
+
+          {/* ITEM 1 */}
+         <div className="flex gap-4">
+          
+          {/* <img src="https://images.pexels.com/photos/29276466/pexels-photo-29276466/free-photo-of-starry-night-over-snowy-norwegian-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={72} height={96} className="object-cover rounded-md"/>  */}
+          <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={72} height={96} className="object-cover rounded-md"/> 
+          <div className="flex flex-col justify-between w-full">
+            {/* TOP SECTION */}
+            <div className="">
+              {/* TITLE  */}
+              <div className=" flex items-center justify-between gap-8">
+                <h3 className="font-semibold">Product Name</h3>
+                <div className="p-1 bg-gray-100 rounded-sm">$550</div>
+              </div>
+              {/* DESC  */}
+              <div className="text-sm text-gray-500"> Available</div>
+            </div>
+            {/* BOTTOM SECTION */}
+            <div className="flex justify-between text-sm">
+              <span className=" text-gray-500">Qty. 2</span>
+              <span className=" text-blue-500">Remove</span>
+            </div>
+          </div>
+        </div>  
+
+          {/* ITEM 2 */}
+         <div className="flex gap-4">
+          
+          {/* <img src="https://images.pexels.com/photos/29276466/pexels-photo-29276466/free-photo-of-starry-night-over-snowy-norwegian-mountains.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={72} height={96} className="object-cover rounded-md"/>  */}
+          <img src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={72} height={96} className="object-cover rounded-md"/> 
+          <div className="flex flex-col justify-between w-full">
+            {/* TOP SECTION */}
+            <div className="">
+              {/* TITLE  */}
+              <div className=" flex items-center justify-between gap-8">
+                <h3 className="font-semibold">Product Name</h3>
+                <div className="p-1 bg-gray-100 rounded-sm">$550</div>
+              </div>
+              {/* DESC  */}
+              <div className="text-sm text-gray-500"> Available</div>
+            </div>
+            {/* BOTTOM SECTION */}
+            <div className="flex justify-between text-sm">
+              <span className=" text-gray-500">Qty. 2</span>
+              <span className=" text-blue-500">Remove</span>
+            </div>
+          </div>
+        </div>  
+        </div>
+         {/* Bottom */}
+        <div className="">
+            <div className="flex items-center justify-between font-semibold">
+              <span>SubTotal</span>
+              <span>1100$</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4 ">
+            Shipping and texes calcuated at chekout
+            </p>
+            <div className=" flex justify-between text-sm">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300"> View Cart</button>
+              <button className="rounded-md py-3 px-4 bg-black text-white"> Check Out</button>
+            </div>
+        </div>
+        </>
+        }
+      
+   
     </div>
+   
+
+    
+    </>
+   
+
   )
 }
 
---
+++ src/components/Slider.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import Link from "next/link"
+import { useState } from "react"
+
+const slides =
+    [
+        {
+
+            id: 1,
+            title: "Summer Sale Collection",
+            description: "Sale! Up to 50% off!",
+            img: "https://images.pexels.com/photos/934063/pexels-photo-934063.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            url: "/",
+            bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+        },
+        {
+
+            id: 2,
+            title: "Winter Sale Collection",
+            description: "Sale! Up to 50% off!",
+            img: "https://images.pexels.com/photos/45982/pexels-photo-45982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            url: "/",
+            bg: "bg-gradient-to-r from-pink-50 to-blue-50",
+        },
+        {
+
+            id: 3,
+            title: "Spring Sale Collection",
+            description: "Sale! Up to 50% off!",
+            img: "https://images.pexels.com/photos/157675/fashion-men-s-individuality-black-and-white-157675.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            url: "/",
+            bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+        }
+
+    ]
+
+const Slider = () => {
+
+    const [current, setCurrent] = useState(0)
+    return (
+
+        <div className="h-[calc(100vh-80px)] overflow-hidden">
+            <div className=" w-max h-full flex transition-all ease-in-out duration-1000 ">
+                {
+                    slides.map((slide) => (
+
+                        <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
+
+                            {/* TEXT CONTAINER  */}
+                            <div className="   xl:w-1/2 xl:h-full flex flex-col gap-8 items-center justify-center 2xl:gap-12 text-center">
+                                <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
+                                <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
+                                <Link href={slide.url}>
+                                    <button className="rounded-md bg-black text-white py-3 px-4 ">SHOP NOW</button>
+                                </Link>
+                            </div>
+
+                            {/* IMAGE CONTAINER  */}
+                            <div className="h-1/2 xl:h-full xl:w-1/2 relative">
+                                <img
+                                    src={slide.img}
+                                    alt=""
+                                    sizes="100%"
+                                    className="object-cover" />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="absolute m-auto left-1/2 bottom-8 flex gap-4 ">
+                {
+                    slides.map((slide, index) => (
+                        <div
+                            className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${current === index ? "scale-150" : ""}`} key={slide.id} >
+                            {current === index && (<div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div >
+                            )}
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+
+    )
+}
+
+export default Slider
